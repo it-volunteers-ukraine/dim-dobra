aadbd0f1c3d9f78a58e900efdf2db460668a241f
Yevhenii2022 <3starosta3@gmail.com>
2023-09-16 15:12:28 +0300

    add house section

--- templates/home.php
+++ templates/home.php
@@ -4,8 +4,13 @@ Template Name: home
 */
 get_header();
 ?>
-<h2 class="home container">home page</h2>
 
-
+  <div class="container ">
+    <section class="house">
+      <h1 class="house__title">Дім Добра: рука допомоги для тих, хто потребує - від людей до тварин</h1> 
+      <button class="button" type="button" aria-label="support the fund"   aria-expanded="false"
+      aria-controls="modal-window">ПІДТРИМАТИ ФОНД</button>
+    </section>
+  </div>
 
 <?php get_footer(); ?>
