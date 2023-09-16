e2b53d6cbbb37c30e3e8adedac7caccdf94e74bc
Yevhenii2022 <3starosta3@gmail.com>
2023-09-16 16:51:19 +0300

    add support section

--- templates/home.php
+++ templates/home.php
@@ -11,6 +11,36 @@ get_header();
       <button class="button" type="button" aria-label="support the fund"   aria-expanded="false"
       aria-controls="modal-window">ПІДТРИМАТИ ФОНД</button>
     </section>
+
+    <section class="support">
+      <h2 class="support__title">Кожен місяць завдяки нашій організації:</h2>    
+      <ul class="support__list">
+        <li>
+          <p class="support__count">50</p>
+          <p class="support__text">сімей отримують разову допомогу</p>
+        </li>
+        <li>
+          <p class="support__count">3000</p>
+          <p class="support__text">людей отримують різного роду допомогу: харчі, одяг, гігієна, тощо.</p>
+        </li>
+        <li>
+          <p class="support__count">100</p>
+          <p class="support__text">котиків ЗСУ на різних напрямках отримують бокси з гігієною, їжею, солодощами, медициною, тощо.</p>
+        </li>
+        <li>
+          <p class="support__count">50</p>
+          <p class="support__text">тварин проживають у нашому притулку і постійно працюють з зоо-психологами та кінологами</p>
+        </li>
+        <li>
+          <p class="support__count">200</p>
+          <p class="support__text">тварин отримують корма від нашого фонду</p>
+        </li>
+        <li>
+          <p class="support__count">100</p>
+          <p class="support__text">врятованих тварин знаходять новий дім</p>
+        </li>
+      </ul>
+    </section>
   </div>
 
 <?php get_footer(); ?>
