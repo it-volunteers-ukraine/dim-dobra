edcc27242dad5547a1189cc63cca9a7564fc12bb
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-09-23 20:39:59 +0300

    header-page fixed

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,3 +1,23 @@
+const openMenuButton = document.getElementById("js-open-menu");
+const closeMenuButton = document.getElementById("js-close-menu");
+const menu = document.getElementById("js-menu");
 
+openMenuButton.addEventListener("click", showMenu);
 
-console.log("header");
+function showMenu() {
+  menu.classList.add("is-open");
+  closeMenuButton.addEventListener("click", hideMenu);
+  menu.addEventListener("click", closeByBgdClick);
+}
+
+function hideMenu() {
+  menu.classList.remove("is-open");
+  closeMenuButton.removeEventListener("click", hideMenu);
+  menu.removeEventListener("click", closeByBgdClick);
+}
+
+function closeByBgdClick(e) {
+  if (e.target === menu) {
+    hideMenu();
+  }
+}
