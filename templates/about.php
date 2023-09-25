914b45c6ec1c3c468d525e6867101dbdc3cc8cdc
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-09-25 23:35:45 +0300

    add template part and edit scss file founding-documents.add new template part need-help-animals

--- templates/about.php
+++ templates/about.php
@@ -17,8 +17,8 @@ get_header();
             назавжди втратив домівку, постраждав внаслідок підриву Каховської ГЕС. </p>
         <div class="command__img"></div>
         <!-- <img class="about__img" src="<?php echo get_template_directory_uri();?> ../src/images/about_mobile.png" alt="about"> -->
-        <p class="about__description description__command">Наша команда волонтерів регулярно віддає свої зусилля, щоб допомогти тим,хто потребує допомоги найбільше.Двічі на місяць ми організовуємо 
-            безкоштовне харчування для переселенців у нашому місті,надаючи продукти харчування, засоби гігієни та одяг. </p>
+        <p class="about__description description__command">Наша команда волонтерів регулярно віддає свої зусилля, щоб допомогти тим,хто потребує допомоги найбільше.
+            Двічі на місяць ми організовуємо безкоштовне харчування для переселенців у нашому місті,надаючи продукти харчування, засоби гігієни та одяг. </p>
         <p class="about__description description__other">Не обмежуючись лише нашим містом, наші волонтери також виїжджають двічі на місяць до Херсонської області, де годують 
             людей гарячими обідами, роздають хліб та продуктові набори. Ми не залишаємо поза увагою літніх людей і стараємося забезпечити їм підтримку та допомогу 
             у побутових потребах. </p>
@@ -29,7 +29,7 @@ get_header();
             </div>
             <div class="phone">
                 <img src="<?php echo get_template_directory_uri();?> ../src/images/carbon_phone.png" alt="email" class="phone__img">
-                <a class="phone__link" href="tel:+380960292222">+380960292222 </a> | <a class="phone__link" href="tel:+380680292222"> +380680292222</a> 
+                <a class="phone__link__first" href="tel:+380960292222">+380960292222 </a>&nbsp|&nbsp <a class="phone__link" href="tel:+380680292222"> +380680292222</a> 
             </div>
         </div>
         <div class="work__img"></div>
@@ -40,10 +40,8 @@ get_header();
         <button class="support__btn button">ПІДТРИМАТИ ФОНД</button>
     </section>
     <!-- documents -->
-    <!-- need__help -->
     <?php get_template_part('template-parts/founding-documents'); ?>
-
-    <?php get_template_part('template-parts/need-help'); ?>
-    
+    <!-- help-->
+    <?php get_template_part('template-parts/need-help-animals'); ?>
    </div>
     <?php get_footer(); ?>
