af6d6bb1ab4a1a4a734feb990c2b1a3efd20883c
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-09-30 20:04:33 +0300

    added form css

--- templates/helpform.php
+++ templates/helpform.php
@@ -5,14 +5,16 @@ Template Name: helpform
 get_header();
 ?>
 <main>
-    <div class="container">
-        <p>Повернутись назад</p>
+    <div class="container back-page__wrap">
+        <p class="back-page__btn">Повернутись назад</p>
     </div>
     <div class="external__wrap">
         <div class="container">
             <div class="internal__wrap">
                 <h1 class="helpform__title">Форма подачі заявки на допомогу</h1>
-                <?php echo do_shortcode('[contact-form-7 id="71b2894" title="help-form"]')?>
+                <div class="helpform">
+                    <?php echo do_shortcode('[contact-form-7 id="71b2894" title="help-form"]')?>
+                </div>
             </div>
         </div>
     </div> 
