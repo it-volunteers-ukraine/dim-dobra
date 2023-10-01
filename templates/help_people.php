b5427a11d7cd4f0d4ee5cf2d78af8b0b42777299
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-10-01 20:46:18 +0300

    Update help_people.php

--- templates/help_people.php
+++ templates/help_people.php
@@ -6,7 +6,10 @@ get_header();
 ?>
 <main>
     <?php get_template_part( 'template-parts/breadcrumbs'); ?>
-    <section class="container"></section>
+    <section class="container">
+        <h1>Фотозвіти допомоги людям</h1>
+    </section>
+    <?php get_template_part( 'template-parts/need-help'); ?>
 </main>
 
 <?php get_footer(); ?>
