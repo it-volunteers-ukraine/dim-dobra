e8caf88efb419414cdcfd0f996ba66803002d61c
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-10-03 15:34:40 +0300

    added css for single-page

--- single.php
+++ single.php
@@ -1,18 +1,18 @@
-<!-- <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package wp-it-volunteers
- */
+ <?php get_header(); ?>
 
-get_header();
-?>
+ <main>
+    <?php get_template_part( 'template-parts/breadcrumbs'); ?>
+	<section>
+		<div class="container">
+			<h1>Допомога Херсонщині від 23.07.2023</h1>
+			<p>Наші волонтери в котрий раз доставили гуманітарну допомогу на Херсонщину: продукти харчування, питну воду, медикаменти, одяг та інші необхідні речі для людей, які потерпіли від кризових ситуацій. Ця місія виявилася особливо важливою, оскільки обстановка на Херсонщині залишається надзвичайно складною через наслідки підриву Каховської ГЕС. Ми вивчаємо потреби місцевого населення та докладаємо всіх можливих зусиль, щоб забезпечити їх допомогою та підтримкою. Крім матеріальної допомоги, наші волонтери також надають психологічну підтримку та спілкуються з людьми, які пережили складні події.</p>
+		</div>
+		<img src="<?php echo get_template_directory_uri()?>/assets/images/command-HD.jpg ?>" alt="">
+	</section>
+    <?php get_template_part( 'template-parts/need-help'); ?>
+</main>
 
-	<main id="primary" class="site-main">
-
-		<?php
+		<!-- <?php
 		while ( have_posts() ) :
 			the_post();
 
@@ -31,10 +31,8 @@ get_header();
 			endif;
 
 		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+		?> -->
 
 <?php
-get_sidebar();
-get_footer(); -->
+
+get_footer();
--- templates/blog-template.php
+++ templates/blog-template.php
@@ -9,18 +9,27 @@ get_header();
     <section class="help__section">
         <div class="container">
 
+            <h1 class="help-people__title"><?php echo get_the_title(); ?></h1>
+
             <?php 
+            $postID = $post->ID;
+            $category = ($postID === 95) ? 3 : 4;
+
             $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
             $args = array(
                 'posts_per_page' => get_option('posts_per_page'),
-                'paged'          => $current_page 
+                'paged'          => $current_page,
+                'tax_query'      => array(
+                    array(
+                        'taxonomy'=> 'category',
+                        'terms' => $category
+                    )
+                )
             );
             query_posts( $args );
             
             $wp_query->is_archive = true;
             $wp_query->is_home = false; ?>
-
-            <h1 class="help-people__title"><?php the_title(); ?></h1>
 
             <div class="posts-list">
             <?php while(have_posts()): the_post(); 
