7dc58f21bf06b6cff28279cc0e9a19b40d44a504
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-10-04 13:47:59 +0300

    part need-help

--- template-parts/need-help-animals.php
+++ template-parts/need-help-animals.php
@@ -1,6 +1,7 @@
-<section class="help-animals">
+<div class="help-animals">
+        <img class="help-animals__img"  src="<?php echo get_template_directory_uri();?> ../assets/images/need-help-desktop.jpg" alt="Потрібна допомога">
         <h3 class="help-animals__title">Потрібна допомога?</h3>  
         <button class="button help-animals__button" type="button" aria-label="apply" aria-expanded="false" aria-controls="modal-window">
             ПОДАТИ ЗАЯВКУ
         </button>  
-    </section>
+</div>
