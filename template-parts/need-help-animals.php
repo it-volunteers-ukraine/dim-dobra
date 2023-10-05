989e731463d7f4005fd7cf5643e45bf46e4cf67b
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-10-05 13:03:27 +0300

    page about(with WP) and footer

--- template-parts/need-help-animals.php
+++ template-parts/need-help-animals.php
@@ -1,6 +1,7 @@
 <div class="help-animals">
-        <img class="help-animals__img"  src="<?php echo get_template_directory_uri();?> ../assets/images/need-help-desktop.jpg" alt="Потрібна допомога">
-        <h3 class="help-animals__title">Потрібна допомога?</h3>  
+
+        <img src="<?php the_field('need-help-img'); ?>" class="help-animals__img" alt="Потрібна допомога">
+        <h3 class="help-animals__title"><?php the_field('need-help-title'); ?></h3>  
         <button class="button help-animals__button" type="button" aria-label="apply" aria-expanded="false" aria-controls="modal-window">
             ПОДАТИ ЗАЯВКУ
         </button>  
