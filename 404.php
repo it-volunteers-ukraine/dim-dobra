c359d69988ebc412ee9d3c65eb57c20eece84798
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-10-11 18:46:52 +0300

    fixed some bugs in page 404

--- 404.php
+++ 404.php
@@ -12,9 +12,9 @@
     <div class="background-404">
     <img class="img-404" src="<?php the_field('error-image', 'option')?>" alt=""></div>
 	<section class="error-404 not-found">
-		<p class="error-text">сталася помилка....</p>
-		<p class="error-oops">oops</p>
-		<a class="error-button" href="<?php echo get_home_url(); ?>">Повернутися на головну</a>
-	</section><!-- .error-404 -->
+		<h2 class="error-text"><?php echo esc_attr(get_field('error-text', 'option')); ?></h2>
+		<h1 class="error-oops"><?php echo esc_attr(get_field('error-oops', 'option')); ?></h1>
+		<a class="error-button" href="<?php echo get_home_url(); ?>"><?php echo esc_attr(get_field('error-button', 'option')); ?></a>
+	</section>
 
-</main><!-- #main -->
+</main>
