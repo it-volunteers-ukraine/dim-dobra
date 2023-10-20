9aab15914fa728d6b2c7cca91713e16f3c7644a3
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-10-20 20:42:44 +0300

    php Intelephense

--- footer.php
+++ footer.php
@@ -1,81 +1,76 @@
 <footer class="footer">
     <section class="main__information">
         <div class="logo">
-        <a href="#">
-                <?php 
-                    if ( has_custom_logo() ) {
-                        echo get_custom_logo();
-                    }
-                ?>                
-            </a>         
+            <a href="#">
+                <?php
+                if (has_custom_logo()) {
+                    echo get_custom_logo();
+                }
+                ?>
+            </a>
         </div>
         <div class="working__hours">
-            <p class="working__days"><?php the_field ('working-days', 'options'); ?></p>
-            <p class="day__off"><?php the_field ('day-off', 'options'); ?></p>
+            <p class="working__days"><?php the_field('working-days', 'options'); ?></p>
+            <p class="day__off"><?php the_field('day-off', 'options'); ?></p>
         </div>
     </section>
     <section class="nav">
         <nav class="footer__nav" id="menu">
-            <?php 
-            wp_nav_menu ( [
+            <?php
+            wp_nav_menu([
                 'theme_locatoin'      => 'footer',
                 'container'           => false,
                 'menu_class'          => 'footer__menu',
                 'menu_id'             => false,
                 'echo'                => true,
                 'items_wrap'          => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-              ] );
-             ?> 
-		</nav>
+            ]);
+            ?>
+        </nav>
     </section>
     <section class="footer__contacts">
         <div class="contacts__container">
             <div class="contacts__text"><?php the_field('contacts-title', 'options'); ?></div>
             <div class="footer__address contacts__flex">
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/geolocation.png" alt="address" class="footer__img">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/geolocation.png" alt="address" class="footer__img">
                 <p class="footer__text"><?php the_field('address', 'options'); ?></p>
             </div>
             <div class="footer__email contacts__flex">
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/carbon_email.png" alt="email" class="footer__img">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carbon_email.png" alt="email" class="footer__img">
                 <a class="footer__link" href="<?php the_field('email'); ?>"><?php the_field('email', 4); ?></a>
             </div>
             <div class="footer__phone contacts__flex">
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/carbon_phone.png" alt="phone" class="footer__img">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carbon_phone.png" alt="phone" class="footer__img">
                 <a class="phone__link__first" href="<?php the_field('first-phone'); ?>"><?php the_field('first-phone', 4); ?> </a>&nbsp|&nbsp
-                <a class="phone__link" href="<?php the_field('second-phone'); ?>"><?php the_field('second-phone', 4); ?></a> 
+                <a class="phone__link" href="<?php the_field('second-phone'); ?>"><?php the_field('second-phone', 4); ?></a>
             </div>
         </div>
-            <div class="mob__social">
-                <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('viber', 2) ); ?>"><svg class="icon-socials"
-                         width="40px" height="40px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#viber"></use>
-                 </svg></a></li>
-                <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('telegram', 2) ); ?>"><svg class="icon-socials"
-                                                width="40px" height="40px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#telegram"></use>
-                </svg></a></li>
-                <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('whatsapp', 2) ); ?>"><svg class="icon-socials"
-                                                width="40px" height="40px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#whatsapp"></use>
-                </svg></a></li>
-            </div> 
-            <div class="tablet__social">   
-                <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('viber', 2) ); ?>"><svg class="icon-socials"
-                         width="32px" height="32px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#viber"></use>
-                 </svg></a></li>
-                <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('telegram', 2) ); ?>"><svg class="icon-socials"
-                                                width="32px" height="32px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#telegram"></use>
-                </svg></a></li>
-                <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('whatsapp', 2) ); ?>"><svg class="icon-socials"
-                                                width="32px" height="32px">
-                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite-logo.svg#whatsapp"></use>
-                </svg></a></li>
-            </div>
-        </section>
+        <div class="mob__social">
+            <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('viber', 2)); ?>"><svg class="icon-socials" width="40px" height="40px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#viber"></use>
+                    </svg></a></li>
+            <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('telegram', 2)); ?>"><svg class="icon-socials" width="40px" height="40px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#telegram"></use>
+                    </svg></a></li>
+            <li class="social__item"><a class="mob-social__link" href="<?php echo esc_attr(get_field('whatsapp', 2)); ?>"><svg class="icon-socials" width="40px" height="40px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#whatsapp"></use>
+                    </svg></a></li>
+        </div>
+        <div class="tablet__social">
+            <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('viber', 2)); ?>"><svg class="icon-socials" width="32px" height="32px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#viber"></use>
+                    </svg></a></li>
+            <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('telegram', 2)); ?>"><svg class="icon-socials" width="32px" height="32px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#telegram"></use>
+                    </svg></a></li>
+            <li class="social__item"><a class="tablet-social__link" href="<?php echo esc_attr(get_field('whatsapp', 2)); ?>"><svg class="icon-socials" width="32px" height="32px">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite-logo.svg#whatsapp"></use>
+                    </svg></a></li>
+        </div>
+    </section>
 </footer>
-<?php wp_footer(); ?>  
+<?php wp_footer(); ?>
 
 </body>
+
 </html>
--- templates/about.php
+++ templates/about.php
@@ -4,43 +4,43 @@ Template Name: About
 */
 get_header();
 ?>
-<?php get_template_part( 'template-parts/breadcrumbs'); ?>
+<?php get_template_part('template-parts/breadcrumbs'); ?>
 
-    <section class="about container">
-        <h1 class="about__title"><?php the_field('about-title'); ?></h1>
-        <p class="about__description description__main"><?php the_field('description-main'); ?></p>
-        <div class="about__img">
-            <img class="image" src="<?php the_field('about-img'); ?>" alt="about">
+<section class="about container">
+    <h1 class="about__title"><?php the_field('about-title'); ?></h1>
+    <p class="about__description description__main"><?php the_field('description-main'); ?></p>
+    <div class="about__img">
+        <img class="image" src="<?php the_field('about-img'); ?>" alt="about">
+    </div>
+    <p class="about__description description__command"><?php the_field('description-command'); ?></p>
+    <p class="about__description description__other"><?php the_field('description-other'); ?></p>
+    <div class="about__contact">
+        <div class="email">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carbon_email.png" alt="email" class="email__img">
+            <a class="email__link" href="mailto:<?php the_field('email'); ?>"><?php the_field('email', 4); ?></a>
         </div>
-        <p class="about__description description__command"><?php the_field('description-command'); ?></p>
-        <p class="about__description description__other"><?php the_field('description-other'); ?></p>
-        <div class="about__contact">
-            <div class="email">
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/carbon_email.png" alt="email" class="email__img">
-                <a class="email__link" href="mailto:<?php the_field('email'); ?>"><?php the_field('email', 4); ?></a>
-            </div>
-            <div class="phone">
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/carbon_phone.png" alt="email" class="phone__img">
-                <a class="phone__link__first" href="tel:+<?php the_field('first-phone'); ?>"><?php the_field('first-phone', 4); ?></a>&nbsp|&nbsp 
-                <a class="phone__link" href="tel:<?php the_field('second-phone'); ?>"><?php the_field('second-phone', 4); ?></a> 
-            </div>
+        <div class="phone">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carbon_phone.png" alt="email" class="phone__img">
+            <a class="phone__link__first" href="tel:+<?php the_field('first-phone'); ?>"><?php the_field('first-phone', 4); ?></a>&nbsp|&nbsp
+            <a class="phone__link" href="tel:<?php the_field('second-phone'); ?>"><?php the_field('second-phone', 4); ?></a>
         </div>
-        <div class="work__img">
-            <img class="image" src="<?php the_field('work-img'); ?>" alt="work">
-        </div>
-        <p class="about__description description__animals"><?php the_field('animals'); ?></p>
-        <p class="about__description description__morehelp"><?php the_field('more-help'); ?></p>
-        <a href="<?php the_field('button-link-support'); ?>" class="support__btn">
-            <button class="button" type="button" aria-label="apply" aria-expanded="false" aria-controls="modal-window">
-              <?php the_field('button-text-support'); ?>
-            </button>
-        </a>
-    </section>
+    </div>
+    <div class="work__img">
+        <img class="image" src="<?php the_field('work-img'); ?>" alt="work">
+    </div>
+    <p class="about__description description__animals"><?php the_field('animals'); ?></p>
+    <p class="about__description description__morehelp"><?php the_field('more-help'); ?></p>
+    <a href="<?php the_field('button-link-support'); ?>" class="support__btn">
+        <button class="button" type="button" aria-label="apply" aria-expanded="false" aria-controls="modal-window">
+            <?php the_field('button-text-support'); ?>
+        </button>
+    </a>
+</section>
 
-    <!-- documents -->
-    <?php get_template_part('template-parts/founding-documents'); ?>
+<!-- documents -->
+<?php get_template_part('template-parts/founding-documents'); ?>
 
-    <!-- help-->
-    <?php get_template_part('template-parts/need-help-animals'); ?>
+<!-- help-->
+<?php get_template_part('template-parts/need-help-animals'); ?>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
