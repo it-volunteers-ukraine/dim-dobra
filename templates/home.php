b9a00627d1c54dee83b170bfb95a36e60b783a6e
Yevhenii2022 <3starosta3@gmail.com>
2023-10-20 16:42:40 +0300

    add tab name

--- templates/home.php
+++ templates/home.php
@@ -5,106 +5,102 @@ Template Name: home
 get_header();
 ?>
 
-  <div class="container ">
-    <section class="house" style="background-image: url('<?php the_field('hero_img_background')?>')">
+<div class="container ">
+  <section class="house" style="background-image: url('<?php the_field('hero_img_background') ?>')">
 
-      <div class="house__title-wraper">
-        <h1 class="house__title"><?php the_field('hero_title'); ?></h1>
-      </div> 
-      
-      <a href="#bank-details" class="house__link">
-        <button class="button house__button" type="button" aria-label="support"  
-        aria-controls="bank-details-section">
-          <?php the_field('hero_btn_text'); ?>
-        </button>
-      </a>
-    </section>
+    <div class="house__title-wraper">
+      <h1 class="house__title"><?php the_field('hero_title'); ?></h1>
+    </div>
 
-    <section class="support">
-      <h2 class="support__title"><?php the_field('support__title'); ?></h2>    
-      <ul class="support__list">
+    <a href="#bank-details" class="house__link">
+      <button class="button house__button" type="button" aria-label="support" aria-controls="bank-details-section">
+        <?php the_field('hero_btn_text'); ?>
+      </button>
+    </a>
+  </section>
+
+  <section class="support">
+    <h2 class="support__title"><?php the_field('support__title'); ?></h2>
+    <ul class="support__list">
 
       <?php
       $support__list = get_field("support__list");
 
-      if($support__list) : ?>
-        <?php foreach($support__list as $support) : 
+      if ($support__list) : ?>
+        <?php foreach ($support__list as $support) :
           $count = $support['support__count'];
           $text = $support['support__text'];
         ?>
           <li class="support__item">
             <p class="support__count"><?php echo $count; ?></p>
-            <p class="support__text"><?php echo $text; ?></p>       
+            <p class="support__text"><?php echo $text; ?></p>
           </li>
         <?php endforeach; ?>
       <?php endif; ?>
 
-      </ul>
-    </section>
+    </ul>
+  </section>
 
-    <section class="about">
-        <h3 class="about__title"><?php the_field('about__title'); ?></h3> 
+  <section class="about">
+    <h3 class="about__title"><?php the_field('about__title'); ?></h3>
 
-        <div class="about__wraper">
-          <p class="about__text">
-            <?php the_field('about_text_first'); ?>
-          <br> <br> 
-            <?php the_field('about_text_second'); ?>
-          </p>  
+    <div class="about__wraper">
+      <p class="about__text">
+        <?php the_field('about_text_first'); ?>
+        <br> <br>
+        <?php the_field('about_text_second'); ?>
+      </p>
 
-          <p class="about__text-tablet">
-            <?php the_field('about_text_first'); ?> 
-            <?php the_field('about_text_second'); ?>
-          <br><br> 
-            <?php the_field('about_text_thirdth'); ?> 
-            <?php the_field('about_text_fourth'); ?>
-          </p>
-  
-        <?php if (have_rows('about_slider')) : ?>
-          <div class="swiper-container">
-            <div class="swiper mySwiper">
-              <div class="swiper-wrapper" style="margin-bottom: 32px">      
+      <p class="about__text-tablet">
+        <?php the_field('about_text_first'); ?>
+        <?php the_field('about_text_second'); ?>
+        <br><br>
+        <?php the_field('about_text_thirdth'); ?>
+        <?php the_field('about_text_fourth'); ?>
+      </p>
 
-            <?php while(have_rows('about_slider')) : the_row(); 
-            $img = get_sub_field('about_slider_img'); ?>
-              <div class="swiper-slide about__img-wrap"> 
-                <img src='<?php echo $img['url']; ?>' alt='<?php echo $img['alt']; ?>'/>
-              </div>
-            <?php endwhile; ?> 
-              </div>
-                <div class="swiper-pagination pagination-pos" style="position: inherit">       
+      <?php if (have_rows('about_slider')) : ?>
+        <div class="swiper-container">
+          <div class="swiper mySwiper">
+            <div class="swiper-wrapper" style="margin-bottom: 32px">
 
-              </div>
-                <div class="pagination-wrap">
-                  <div class="custom-prev-icon"></div>
-                  <div class="custom-next-icon"></div>              
+              <?php while (have_rows('about_slider')) : the_row();
+                $img = get_sub_field('about_slider_img'); ?>
+                <div class="swiper-slide about__img-wrap">
+                  <img src='<?php echo $img['url']; ?>' alt='<?php echo $img['alt']; ?>' />
                 </div>
-            </div>       
+              <?php endwhile; ?>
+            </div>
+            <div class="swiper-pagination pagination-pos" style="position: inherit"></div>
+            <div class="pagination-wrap">
+              <div class="custom-prev-icon"></div>
+              <div class="custom-next-icon"></div>
+            </div>
           </div>
-        <?php endif; ?>
+        </div>
+      <?php endif; ?>
+    </div>
 
-      </div>
+    <p class="about__text">
+      <?php the_field('about_text_thirdth'); ?>
+      <br> <br>
+      <?php the_field('about_text_fourth'); ?>
+    </p>
+  </section>
 
-        <p class="about__text">
-            <?php the_field('about_text_thirdth'); ?> 
-          <br> <br> 
-            <?php the_field('about_text_fourth'); ?>
-        </p>    
-    </section>
+</div>
 
-  </div>
+<?php get_template_part('template-parts/founding-documents'); ?>
 
-    <?php get_template_part( 'template-parts/founding-documents'); ?>
+<div class="container ">
 
-  <div class="container ">
+  <!-- section news  -->
+  <section class="news">
+    <h3 class="news__title"><?php the_field('news__title'); ?></h3>
 
-    <!-- section news  -->
-    <section class="news">
-      <h3 class="news__title"><?php the_field('news__title'); ?></h3>
-
-      <div class="swiper-container news__hide">
-        <div class="swiper mySwiper">
-          <div class="swiper-wrapper">
+    <div class="swiper-container news__hide">
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
 
           <?php
           // Запит для категорії "help-people"
@@ -132,35 +128,35 @@ get_header();
 
           // Вивід постів
           foreach ($merged_posts as $post) {
-          setup_postdata($post);
-          $title = get_the_title();
+            setup_postdata($post);
+            $title = get_the_title();
           ?>
 
             <div class="swiper-slide swiper-hover">
               <a href="<?php the_permalink(); ?>">
                 <div class="news__wraper-img">
                   <?php the_post_thumbnail(); ?>
-                </div>           
+                </div>
                 <div class="news__wraper">
                   <p class="news__text"><?php the_field('subtext_posts'); ?></p>
                   <p class="news__text"><?php the_time('d.m.Y'); ?></p>
                 </div>
-                  <h4 class="news__subTitle"><?php echo $title; ?></h4>
+                <h4 class="news__subTitle"><?php echo $title; ?></h4>
               </a>
             </div>
 
           <?php
-            }
-            wp_reset_postdata();            
+          }
+          wp_reset_postdata();
           ?>
 
-          </div>
-            <div class="swiper-pagination" style="position: inherit"></div>
         </div>
+        <div class="swiper-pagination" style="position: inherit"></div>
       </div>
-      
-      <div class="news__gallery">
-        <div class="news__flexWraper" >
+    </div>
+
+    <div class="news__gallery">
+      <div class="news__flexWraper">
 
         <?php
         // Запит для категорії "help-people"
@@ -192,97 +188,96 @@ get_header();
           $title = get_the_title();
         ?>
 
-        <div class="news__wraper">
-          <a href="<?php the_permalink(); ?>">
-            <div class="news__wraper-img">
-              <?php the_post_thumbnail(); ?>
-            </div> 
-                
-            <div class="news__text">
-              <p><?php the_field('subtext_posts'); ?></p>
-              <p><?php the_time('d.m.Y'); ?></p>
-            </div>
-            <h4 class="news__subTitle"><?php echo $title; ?></h4>
-          </a>
-        </div>
+          <div class="news__wraper">
+            <a href="<?php the_permalink(); ?>">
+              <div class="news__wraper-img">
+                <?php the_post_thumbnail(); ?>
+              </div>
+
+              <div class="news__text">
+                <p><?php the_field('subtext_posts'); ?></p>
+                <p><?php the_time('d.m.Y'); ?></p>
+              </div>
+              <h4 class="news__subTitle"><?php echo $title; ?></h4>
+            </a>
+          </div>
 
         <?php
-      }
+        }
 
-      wp_reset_postdata();
-      ?>
+        wp_reset_postdata();
+        ?>
 
-        </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <!-- section bank-details  -->
-    <section id="bank-details">
-      <h3 class="bank-details__title"><?php the_field('bank_details_title'); ?></h3>
-      
-      <?php if (have_rows('bank_details_tabs')) : ?>
+  <!-- section bank-details  -->
+  <section id="bank-details">
+    <h3 class="bank-details__title"><?php the_field('bank_details_title'); ?></h3>
+
+    <?php if (have_rows('bank_details_tabs')) : ?>
       <div class="tabs">
         <ul>
-          <li> 
-            <span class="tabs-span1">Для переказу у гривнях</span> 
-            <span class="tabs-span2">UAH</span> 
+          <li>
+            <span class="tabs-span1"><?php the_field('tab_name_uah'); ?></span>
+            <span class="tabs-span2">UAH</span>
           </li>
-          <li>          
-            <span class="tabs-span1">Для переказу у доларах</span> 
-            <span class="tabs-span2">USD</span> 
+          <li>
+            <span class="tabs-span1"><?php the_field('tab_name_usd'); ?></span>
+            <span class="tabs-span2">USD</span>
           </li>
-          <li>          
-            <span class="tabs-span1">Для переказу у євро</span> 
-            <span class="tabs-span2">EUR</span> 
-          </li>      
+          <li>
+            <span class="tabs-span1"><?php the_field('tab_name_eur'); ?></span>
+            <span class="tabs-span2">EUR</span>
+          </li>
         </ul>
 
         <div>
-          <?php while(have_rows('bank_details_tabs')) : the_row(); 
-          $img = get_sub_field('qr-code'); ?>
+          <?php while (have_rows('bank_details_tabs')) : the_row();
+            $img = get_sub_field('qr-code'); ?>
 
-          <div class="tabs__wraper">
-            <div class="tabs__flexWraper">
-              <div style="padding-top: 0">
-                <div class="tabs__textWraper" style="padding-top: 0">
-                  <p class="bank-details__text-first"><?php the_sub_field('recipient_first') ?></p>     
-                  <p class="bank-details__text-second"><?php the_sub_field('recipient_second') ?></p>     
+            <div class="tabs__wraper">
+              <div class="tabs__flexWraper">
+                <div style="padding-top: 0">
+                  <div class="tabs__textWraper" style="padding-top: 0">
+                    <p class="bank-details__text-first"><?php the_sub_field('recipient_first') ?></p>
+                    <p class="bank-details__text-second"><?php the_sub_field('recipient_second') ?></p>
+                  </div>
+                  <div class="tabs__textWraper" style="padding-top: 0">
+                    <p class="bank-details__text-first"><?php the_sub_field('edrpou_first') ?></p>
+                    <p class="bank-details__text-second"><?php the_sub_field('edrpou_second') ?></p>
+                  </div>
+                  <div class="tabs__textWraper" style="padding-top: 0">
+                    <p class="bank-details__text-first"><?php the_sub_field('bank_first') ?></p>
+                    <p class="bank-details__text-second"><?php the_sub_field('bank_second') ?></p>
+                  </div>
+                  <div class="tabs__textWraper" style="padding-top: 0">
+                    <p class="bank-details__text-first"><?php the_sub_field('iban_first') ?></p>
+                    <p class="bank-details__text-second"><?php the_sub_field('iban_second') ?></p>
+                  </div>
                 </div>
-                <div class="tabs__textWraper" style="padding-top: 0">
-                  <p class="bank-details__text-first"><?php the_sub_field('edrpou_first') ?></p>     
-                  <p class="bank-details__text-second"><?php the_sub_field('edrpou_second') ?></p> 
+                <div class="tabs__wraper-img">
+                  <img class="tabs-img" src='<?php echo $img['url']; ?>' alt='<?php echo $img['alt']; ?>' />
                 </div>
-                <div class="tabs__textWraper" style="padding-top: 0">    
-                  <p class="bank-details__text-first"><?php the_sub_field('bank_first') ?></p>     
-                  <p class="bank-details__text-second"><?php the_sub_field('bank_second') ?></p>
-                </div>
-                <div class="tabs__textWraper" style="padding-top: 0">      
-                  <p class="bank-details__text-first"><?php the_sub_field('iban_first') ?></p>     
-                  <p class="bank-details__text-second"><?php the_sub_field('iban_second') ?></p>
-                </div>   
               </div>
-              <div class="tabs__wraper-img"> 
-                <img class="tabs-img" src='<?php echo $img['url']; ?>' alt='<?php echo $img['alt']; ?>'/>
-              </div>  
-            </div>        
-            <div class="tabs__wraper-btn" style="padding-top: 0">
-              <a href="<?php the_sub_field('bank_link') ?>" target="_blank">
-                <button class="button tabs-button" type="button" aria-label="support the fund"   aria-expanded="false"
-                aria-controls="modal-window"><?php the_sub_field('tabs_button') ?>
-                </button>
-              </a>
+              <div class="tabs__wraper-btn" style="padding-top: 0">
+                <a href="<?php the_sub_field('bank_link') ?>" target="_blank">
+                  <button class="button tabs-button" type="button" aria-label="support the fund" aria-expanded="false" aria-controls="modal-window"><?php the_sub_field('tabs_button') ?>
+                  </button>
+                </a>
+              </div>
             </div>
-          </div> 
 
           <?php endwhile; ?>
-        </div>                
+        </div>
       </div>
 
-      <?php endif; ?>
-    </section>
+    <?php endif; ?>
+  </section>
 
-    <?php get_template_part( 'template-parts/need-help'); ?>
+  <?php get_template_part('template-parts/need-help'); ?>
 
-  </div>
+</div>
 
 <?php get_footer(); ?>
