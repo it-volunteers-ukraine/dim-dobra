7b0f4229e176503dac2c1ef3da1f40dc3dafae0e
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-11-03 20:59:30 +0200

    fixed bugs in the swiper at the home page

--- src/scripts/template-scripts/home.js
+++ src/scripts/template-scripts/home.js
@@ -1,85 +1,95 @@
 const homeSlyder = new Swiper(".mySwiper", {
-	slidesPerView: 1,
-	slidesPerView: "auto",
-	spaceBetween: 20,
-	loop: true,
-	grabCursor: true,
-	autoHeight: true,
-	speed: 600,
-	keyboard: {
-		enabled: true,
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	navigation: {
-		nextEl: ".custom-next-icon",
-		prevEl: ".custom-prev-icon",
-	},
-	breakpoints: {
-		768: {
-			pagination: {
-				type: "fraction",
-			},
-		},
-	},
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  autoHeight: true,
+  speed: 600,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      navigation: {
+        nextEl: ".custom-next-icon",
+        prevEl: ".custom-prev-icon",
+      },
+      pagination: {
+        el: ".swiper_pagination",
+        clickable: true,
+        type: "fraction",
+      },
+    },
+  },
+});
+
+const myPostSwiper = new Swiper(".myPostSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    slideToClickedSlide: true,
+  },
 });
 
 jQuery.noConflict();
 
 jQuery(document).ready(function ($) {
-	$(".tabs").lightTabs();
+  $(".tabs").lightTabs();
 });
 
 (function ($) {
-	jQuery.fn.lightTabs = function (options) {
-		var createTabs = function () {
-			tabs = this;
-			i = 0;
-			showPage = function (i) {
-				$(tabs).children("div").children("div").hide();
-				$(tabs).children("div").children("div").eq(i).show();
-				$(tabs).children("ul").children("li").removeClass("active");
-				$(tabs).children("ul").children("li").eq(i).addClass("active");
-			};
-			showPage(0);
-			$(tabs)
-				.children("ul")
-				.children("li")
-				.each(function (index, element) {
-					$(element).attr("data-page", i);
-					i++;
-				});
-			$(tabs)
-				.children("ul")
-				.children("li")
-				.click(function () {
-					showPage(parseInt($(this).attr("data-page")));
-				});
-		};
-		return this.each(createTabs);
-	};
+  jQuery.fn.lightTabs = function (options) {
+    var createTabs = function () {
+      tabs = this;
+      i = 0;
+      showPage = function (i) {
+        $(tabs).children("div").children("div").hide();
+        $(tabs).children("div").children("div").eq(i).show();
+        $(tabs).children("ul").children("li").removeClass("active");
+        $(tabs).children("ul").children("li").eq(i).addClass("active");
+      };
+      showPage(0);
+      $(tabs)
+        .children("ul")
+        .children("li")
+        .each(function (index, element) {
+          $(element).attr("data-page", i);
+          i++;
+        });
+      $(tabs)
+        .children("ul")
+        .children("li")
+        .click(function () {
+          showPage(parseInt($(this).attr("data-page")));
+        });
+    };
+    return this.each(createTabs);
+  };
 })(jQuery);
 
 // Затримка для плавної прокрутки до секції
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-	anchor.addEventListener("click", function (e) {
-		e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-		const targetId = this.getAttribute("href").substring(1);
-		const targetElement = document.getElementById(targetId);
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
 
-		if (targetElement) {
-			const offsetTop = targetElement.offsetTop;
-			const delay = 300;
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop;
+      const delay = 300;
 
-			setTimeout(() => {
-				window.scrollTo({
-					top: offsetTop,
-					behavior: "smooth",
-				});
-			}, delay);
-		}
-	});
+      setTimeout(() => {
+        window.scrollTo({
+          top: offsetTop,
+          behavior: "smooth",
+        });
+      }, delay);
+    }
+  });
 });
